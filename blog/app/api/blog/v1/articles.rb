0efa0285796555dfd0b1e1cd0678bba5b4ac798a
drewterry <drew.terry@skookum.com>
2018-02-27 16:03:16 -0500

    added create article

--- blog/app/api/blog/v1/articles.rb
+++ blog/app/api/blog/v1/articles.rb
@@ -10,12 +10,56 @@ module Blog
           present Article.all, with: Blog::Entities::Index
         end
 
+        desc 'Create new article.'
+        params do
+          requires :title, type: String
+          requires :text, type: String
+          # Include a "don't save flag" for API testing?
+          optional :test, type: Boolean
+        end
+        post do
+          declared_params = declared(params)
+          @article = Article.new(title: declared_params[:title],
+                                 text: declared_params[:text])
+
+          # Only save if test is false or missing
+          unless declared_params[:test]
+            unless @article.save
+              error! @article.errors, 400
+            end
+          end
+
+          # Return the new article
+          present @article, with: Blog::Entities::Article
+        end
+
         # Methods requiring an article id
         route_param :id do
           desc 'Get specific article.'
           get do
             present Article.find(params[:id]), with: Blog::Entities::Article
           end
+=begin
+          desc 'Update article.'
+          params do
+            requires :title, type: String
+            requires :text, type: String
+            # Include a "don't save flag" for API testing?
+            optional :test, type: Boolean
+          end
+          patch do
+            @article = Article.find(params[:id])
+
+            unless @article.update(article_params)
+
+            end
+
+            present @article, with: Blog::Entities::Article
+          end
+
+          desc 'Destroy article.'
+
+=end
 
           resource :comments do
             desc 'Get all comments'

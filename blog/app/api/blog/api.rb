5a448c0877eaac4df9b1e184501d19c757e07394
drewterry <drew.terry@skookum.com>
2018-02-28 16:22:20 -0500

    enabled swagger for documentation

--- blog/app/api/blog/api.rb
+++ blog/app/api/blog/api.rb
@@ -1,8 +1,13 @@
+require 'grape-swagger'
+
 module Blog
   ## Load the various API versions
   class API < Grape::API
     prefix 'api'
 
     mount Blog::V1::API
+    add_swagger_documentation info: {
+      title: "DT's blog API"
+    }
   end
 end

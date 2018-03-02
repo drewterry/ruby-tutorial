35a92c915fa3cce076a27101116c4b1055d528a3
drewterry <drew.terry@skookum.com>
2018-03-02 15:15:26 -0500

    added OAuth for FB!

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -3,5 +3,39 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable, :confirmable
+         :validatable, :confirmable, 
+         :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    # if oauth_user exists, do nothing
+    unless user
+      puts 'no existing oauth'
+      user = User.find_by(email: auth.info.email)
+      # if normal user exists, dont create a new one
+      unless user
+        puts 'new user'
+        user = User.new
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+      end
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.first_name = auth.info.first_name if user.first_name.blank?
+      user.last_name = auth.info.last_name if user.last_name.blank?
+      user.display_name = "#{auth.info.first_name} #{auth.info.last_name}" if user.display_name.blank?
+
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+      user.save
+    end
+    puts user.inspect
+    user
+  end
+
+  # Allow accounts to be email-less if using OAuth
+  def email_required?
+    true unless provider?
+  end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   get 'welcome/index'
 
   root 'welcome#index'
 
   resources :articles do
     resources :comments
+  end
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   mount Blog::API, at: '/'

96fe0f9ed6280d05cc762b51ac82d9296392de0f
drewterry <drew.terry@skookum.com>
2018-03-02 08:36:04 -0500

    added user fields and updated seed

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301211715) do
+ActiveRecord::Schema.define(version: 20180302133337) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,15 +28,6 @@ ActiveRecord::Schema.define(version: 20180301211715) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "display_name"
-    t.boolean "admin"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +42,9 @@ ActiveRecord::Schema.define(version: 20180301211715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -1,7 +1,3 @@
-User.create!([
-  {email: "nick.terry@gmail.com", encrypted_password: "$2a$11$r3pxxFVQVd0LyvL7dqc5IOdqA6T7LWxfj5dYX62O7Q8Pdg/swgxly", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 8, current_sign_in_at: "2018-03-01 20:24:20", last_sign_in_at: "2018-03-01 20:23:24", current_sign_in_ip: "172.17.0.1", last_sign_in_ip: "172.17.0.1", admin: nil},
-  {email: "drew.terry@skookum.com", encrypted_password: "$2a$11$8bg80Xxgmt.hcNeNTA2KeeTCSZwbxU9edhW5sJVAQ3u1l/xEV1QYy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2018-03-01 20:24:04", last_sign_in_at: "2018-03-01 20:23:54", current_sign_in_ip: "172.17.0.1", last_sign_in_ip: "172.17.0.1", admin: nil}
-])
 Article.create!([
   {title: "Drew is ", text: "an awesome intern"},
   {title: "testing form", text: "reusability is awesome!"},

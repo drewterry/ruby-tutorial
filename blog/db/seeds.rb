f27d143f543b7e7b9e209e090cbb3594e8500822
drewterry <drew.terry@skookum.com>
2018-03-02 09:43:24 -0500

    setup figaro for environment variables and added admin account to db:seed

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -4,9 +4,17 @@ Article.create!([
   {title: "New Postman Blog Entry", text: "This is a blog entry created via the Blog API."},
   {title: "Updated Postman Blog Entry", text: "This is a blog entry updated via the Blog API."}
 ])
+
 Comment.create!([
   {commenter: "Drew", body: "yea he is!", article_id: 1},
   {commenter: "Postman", body: "Testing new comment.", article_id: 2},
   {commenter: "Postman", body: "Testing new comment.", article_id: 1},
   {commenter: "nick.terry@gmail.com", body: "but seriously.", article_id: 1}
 ])
+
+User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |admin|
+  admin.password = Rails.application.secrets.admin_password
+  admin.password_confirmation = Rails.application.secrets.admin_password
+  admin.admin = true
+  admin.save
+end
